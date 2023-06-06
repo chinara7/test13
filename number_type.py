70ff61a4c84edcb5f5c66f83cb80281529fc9aeb
Chinara_Huseyn <80186374+chinara7@users.noreply.github.com>
2023-06-06 20:51:09 +0400

    number type fayli gonderildi

---
+++ number_type.py
@@ -0,0 +1,25 @@
+a = 6556561
+# print(type(a))
+b = 3.6556
+# print(type(b))
+
+x, y = 64, 3
+# + - * /  //  %
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(type(x/y))
+print(x//y)
+print(x % y)
+print(x**y)
+print(x**(1/2))
+
+
+num = 765
+teklik = num % 10
+print(teklik)
+onluq = num//10-(num//100)*10
+print(onluq)
+yuzluk = num//100
+print(yuzluk)
